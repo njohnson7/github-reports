ab9e9a71324a3785c4e54a20c5bc2d0514115ad6
Nick Johnson <njohnson7@users.noreply.github.com>
2018-09-03 19:32:52 -0700

    added ruby Logger

--- lib/reports/github_api_client.rb
+++ lib/reports/github_api_client.rb
@@ -1,16 +1,22 @@
 require 'faraday'
 require 'json'
+require 'logger'
 
 module Reports
   User = Struct.new :name, :location, :public_repos
 
   class GitHubAPIClient
+    def initialize
+      @logger       = Logger.new STDOUT
+      @logger.level = Logger::DEBUG
+    end
+
     def user_info(username)
-      url = "https://api.github.com/users/#{username}"
+      url        = "https://api.github.com/users/#{username}"
       start_time = Time.now
-      response = Faraday.get url
-      duration = Time.now - start_time
-      puts '-> %s %s %d (%.3f s)' % [url, 'GET', response.status, duration]
+      response   = Faraday.get url
+      duration   = Time.now - start_time
+      @logger.debug { '-> %s %s %d (%.3f s)' % [url, 'GET', response.status, duration] }
       data = JSON.parse response.body
       User.new data['name'], data['location'], data['public_repos']
     end
