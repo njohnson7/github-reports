569f93ec4445a82a76b3bc89a30967bd089ef67f
Nick Johnson <njohnson7@users.noreply.github.com>
2018-09-04 14:57:58 -0700

    added authentication middleware

--- lib/reports/cli.rb
+++ lib/reports/cli.rb
@@ -42,7 +42,7 @@ module Reports
 
     private
       def client
-        @client ||= GitHubAPIClient.new(ENV['GITHUB_TOKEN'])
+        @client ||= GitHubAPIClient.new
       end
   end
 end
--- lib/reports/github_api_client.rb
+++ lib/reports/github_api_client.rb
@@ -2,58 +2,53 @@ require 'faraday'
 require 'json'
 require 'logger'
 require_relative 'middleware/logging'
+require_relative 'middleware/authentication'
 
 module Reports
   class Error < StandardError; end
   class NonexistentUser < Error; end
   class RequestFailure < Error; end
+  class AuthenticationFailure < Error; end
 
   User = Struct.new :name, :location, :public_repos
   Repo = Struct.new :name, :url
 
-  VALID_STATUS_CODES = [200, 302, 403, 404, 422]
+  VALID_STATUS_CODES = [200, 302, 401, 403, 404, 422]
 
   class GitHubAPIClient
-    def initialize(token)
-      @token = token
-    end
-
     def user_info(username)
-      headers    = { 'Authorization' => "token #{@token}" }
-      url        = "https://api.github.com/users/#{username}"
-      response   = connection.get(url, nil, headers)
+      url      = "https://api.github.com/users/#{username}"
+      response = connection.get(url)
+
       if !VALID_STATUS_CODES.include? response.status
         raise RequestFailure, JSON.parse(response.body)['message']
-      end
-      if response.status == 401
-        raise AuthenticationFailure, "Authentication Failed. Please set the 'GITHUB_TOKEN' environment variable to a valid Github access token."
       end
       if response.status == 404
         raise NonexistentUser, "'#{username}' does not exist"
       end
+
       data = JSON.parse response.body
       User.new data['name'], data['location'], data['public_repos']
     end
 
     def public_repos_for_user(username)
-      headers  = { 'Authorization' => "token #{@token}" }
       url      = "https://api.github.com/users/#{username}/repos"
-      response = connection.get(url, nil, headers)
+      response = connection.get(url)
+
       if !VALID_STATUS_CODES.include? response.status
         raise RequestFailure, JSON.parse(response.body)['message']
-      end
-      if response.status == 401
-        raise AuthenticationFailure, "Authentication Failed. Please set the 'GITHUB_TOKEN' environment variable to a valid Github access token."
       end
       if response.status == 404
         raise NonexistentUser, "'#{username}' does not exist"
       end
+
       data = JSON.parse response.body
       data.map { |repo_data| Repo.new repo_data['full_name'], repo_data['url'] }
     end
 
     def connection
       @connection ||= Faraday::Connection.new do |builder|
+        builder.use Middleware::Authentication
         builder.use Middleware::Logging
         builder.adapter Faraday.default_adapter
       end
